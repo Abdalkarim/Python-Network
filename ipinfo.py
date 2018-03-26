0aeb5557e97f27cf5bedbe999d5e95f67d97cdfc
Mahmoud Abd Alkarim <abdalkarim.mail@gmail.com>
2018-03-27 01:31:40 +0200

    Create ipinfo.py

---
+++ ipinfo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# coding:utf-8
+# About of IP address
+# Author : Mahmoud Abd Alkarim(@Maakthon)
+
+from urllib2 import urlopen
+from json import load
+
+url  = r"http://ipinfo.io/json"
+res  = urlopen(url)
+data = load(res)
+
+ip       = data['ip']
+org      = data['org']
+city     = data['city']
+region   = data['region']
+country  = data['country']
+location = data['loc']
+
+print('''
+_______________________________
+
+|   IP address information    |
+_______________________________
+|
+| IP address   : {0}
+| City         : {1}
+| Region       : {2}
+| Country      : {3}
+| Location     : {4}
+| Organization : {5}
+| 
+'''.format(ip,city,region,country,location,org))
